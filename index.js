294e42018e0a377e23191942e0e8ff411fa8f07c
Lawrence <lawrence@binance.com>
2023-02-18 17:21:10 +0800

    Complete exercise 3.9-3.11

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', (req) => {return JSON.stringify(req.body)})
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+app.use(express.static('build'))
 
 persons = [
     {
@@ -51,7 +54,7 @@ app.get('/info', (request, response) => {
     response.send(`<div>Phonebook has info for ${persons.length} people</div><div>${Date()}</div>`)
 })
 
-app.delete('/api/person/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(p => p.id !== id)
     response.status(204).end()
@@ -85,7 +88,7 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
